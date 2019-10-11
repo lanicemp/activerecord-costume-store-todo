034bf6cc19e32c87632df264d04e36ad89bc5f46
LaNice Powell <lanicemp@aol.com>
2019-10-11 10:33:32 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,4 +6,16 @@
 
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
-# value types according to the spec
+# value types according to the spe
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    def change
+
+        create_table :costumes do |t|
+        t.string :name
+        t.float :price 
+        t.string :size 
+        t.string :image_url 
+        t.timestamps
+        end 
+    end
+end 
